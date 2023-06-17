dffe6435f7c86d89e57325c08cbbeebc2c459bb8
matt-flaig <hi@mattflaig.com>
2023-06-17 02:54:34 -0700

    Re-wrote module to use Fetch instead of system.emit

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ class ModuleInstance extends InstanceBase {
 
 	async init(config) {
 		this.config = config
+		console.log(config)
 
 		this.updateStatus(InstanceStatus.Ok)
 
@@ -64,145 +65,104 @@ class ModuleInstance extends InstanceBase {
 		UpdateVariableDefinitions(this)
 	}
 	
-	async sendAction(action) {
-		try {
-			let self = this;
-			let options = action.options;
-			
-			let host = self.config.host;
-			let port = 80;
-			let psk = self.config.psk;
-			
-			let service = null;
-			let method = null;
-			let params = null;
-			
-			switch (action.action) {
-				case 'power_on':
-					service = 'system';
-					method = 'setPowerStatus';
-					params = {status: true};
-					break;
-				case 'power_off':
-					service = 'system';
-					method = 'setPowerStatus';
-					params = {status: false};
-					break;
-				case 'volume_up':
-					service = 'audio';
-					method = 'setAudioVolume';
-					params = {target: 'speaker', volume: '+1'};
-					break;
-				case 'volume_down':
-					service = 'audio';
-					method = 'setAudioVolume';
-					params = {target: 'speaker', volume: '-1'};
-					break;
-				case 'volume_mute':
-					service = 'audio';
-					method = 'setAudioMute';
-					params = {status: true};
-					break;
-				case 'volume_unmute':
-					service = 'audio';
-					method = 'setAudioMute';
-					params = {status: false};
-					break;
-				case 'change_external_input':
-					service = 'avContent';
-					method = 'setPlayContent';
-					let uri = 'extInput:' + options.kind + '?port=' + options.port;
-					params = {uri: uri};
-					break;
-				default:
-					break;
-			}
-			
-			let cmdObj = {};
-			cmdObj.method = method;
-			cmdObj.version = '1.0';
-			cmdObj.id = 1;
-			cmdObj.params = [params];
-			
-			self.postRest('/sony/' + service, host, port, cmdObj)
-			.then(function(arrResult) {
-				if (arrResult[2].error) {
-					//throw an error to the log
-					self.log('error', arrResult[2].error[1]);
-				}else{
-					self.updateStatus(InstanceStatus.Ok)
+	async sendAction(event) {
+		let self = this;
+		let options = event.options;
+		
+		let host = self.config.host;
+		let port = 80;
+		let psk = self.config.psk;
+		
+		let service = null;
+		let method = null;
+		let params = null;
+		
+		switch (event.actionId) {
+			case 'power':
+				switch (options.power) {
+					case 'power_on':
+						service = 'system';
+						method = 'setPowerStatus';
+						params = {status: true};
+						break;
+					case 'power_off':
+						service = 'system';
+						method = 'setPowerStatus';
+						params = {status: false};
+						break;
 				}
-			})
-			.catch(function(arrResult) {
-				self.updateStatus(InstanceStatus.Error)
-			});
-		} catch (error) {
-			this.log('debug', `Action ${action} failed (${error})`)
+				break;
+			case 'volume':
+				switch (options.volume) {
+					case 'volume_up':
+						service = 'audio';
+						method = 'setAudioVolume';
+						params = {target: 'speaker', volume: '+1'};
+						break;
+					case 'volume_down':
+						service = 'audio';
+						method = 'setAudioVolume';
+						params = {target: 'speaker', volume: '-1'};
+						break;
+					case 'volume_mute':
+						service = 'audio';
+						method = 'setAudioMute';
+						params = {status: true};
+						break;
+					case 'volume_unmute':
+						service = 'audio';
+						method = 'setAudioMute';
+						params = {status: false};
+						break;
+				}
+				break;
+			case 'changeInput':
+				service = 'avContent';
+				method = 'setPlayContent';
+				let uri = 'extInput:' + options.kind + '?port=' + options.port;
+				params = {uri: uri};
+				break;
+			default:
+				break;
 		}
+		
+		let cmdObj = {};
+		cmdObj.method = method;
+		cmdObj.version = '1.0';
+		cmdObj.id = 1;
+		cmdObj.params = [params];
+		
+		self.doRest('POST', '/sony/' + service, host, port, cmdObj)
+		.then(function(res) {
+			if(res.ok){
+				self.updateStatus(InstanceStatus.Ok)
+			}else{
+				console.debug('Connection Error: ' + res);
+				self.updateStatus(InstanceStatus.Error)
+			}
+		})
+		.catch(function(error) {
+			console.debug(error)
+			self.updateStatus(InstanceStatus.Error)
+		});
+	
 	}
 	
-	getRest(cmd, host, port) {
+	async doRest(method, cmd, host, port, body) {
 		var self = this;
-		return self.doRest('GET', cmd, host, port, {});
-	};
-	
-	postRest(cmd, host, port, body) {
-		var self = this;
-		return self.doRest('POST', cmd, host, port, body);
-	};
-	
-	doRest(method, cmd, host, port, body) {
-		var self = this;
-		var url = 'http://' + self.config.host + cmd;
+		var url = 'http://' + (host ? host : self.config.host) + cmd;
+		console.log(url)
+		console.log(body)
 		
-		let extra_headers = {};
-		extra_headers['X-Auth-PSK'] = self.config.psk;
-		
-		return new Promise(function(resolve, reject) {
-			
-			function handleResponse(err, result) {
-				if (err === null && typeof result === 'object' && result.response.statusCode === 200) {
-					// A successful response
-					
-					var objJson = result.data;
-					
-					resolve([ host, port, objJson ]);
-				} else {
-					// Failure. Reject the promise.
-					var message = 'Unknown error';
-					
-					if (result !== undefined) {
-						if (result.response !== undefined) {
-							message = result.response.statusCode + ': ' + result.response.statusMessage;
-						} else if (result.error !== undefined) {
-							// Get the error message from the object if present.
-							message = result.error;
-						}
-					}
-					
-					reject([ host, port, message ]);
-				}
-			}
-			
-			switch(method) {
-				case 'POST':
-					self.system.emit('rest', url, body, function(err, result) {
-						handleResponse(err, result);
-					}, extra_headers);
-					break;
-				case 'GET':
-					self.system.emit('rest_get', url, function(err, result) {
-						handleResponse(err, result);
-					}, extra_headers);
-					break;
-				default:
-					throw new Error('Invalid method');
-					break;
-			}
-			
-		});
-	};
+		let headers = {};
+		headers['X-Auth-PSK'] = self.config.psk;
 
+		return fetch(url, {
+				method: method == "GET" ? "GET" : "POST",
+				headers: headers,
+				body: body
+			});
+	};
 
 }
 
